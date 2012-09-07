c716275865782d4005931503cf1ba17cb11e6560
Cristian Rinaldi <csrinaldi@gmail.com>
2012-09-07 18:19:01 -0300

    Soporte para la version vieja de IndexDB con seteo de version

--- localstorage/fonts.js
+++ localstorage/fonts.js
@@ -1,13 +1,14 @@
-<script type="text/javascript">
-  WebFontConfig = {
-    google: { families: [ 'Oranienbaum::cyrillic-ext,latin,latin-ext', 'Life+Savers::latin,latin-ext', 'Playball::latin,latin-ext', 'Oregano::latin,latin-ext', 'Lemon::latin' ] }
-  };
-  (function() {
+WebFontConfig = {
+    google: {
+        families: [ 'Oranienbaum::cyrillic-ext,latin,latin-ext', 'Life+Savers::latin,latin-ext', 'Playball::latin,latin-ext', 'Oregano::latin,latin-ext', 'Lemon::latin' ]
+    }
+};
+(function() {
     var wf = document.createElement('script');
     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
     wf.type = 'text/javascript';
     wf.async = 'true';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
-  })(); </script>
+})();
